2aa9d92bf54608642f564fe65131d11e4bbb8023
Rodrigo050705 <135657705+Rodrigo050705@users.noreply.github.com>
2023-10-05 20:24:26 -0300

    Add files via upload

---
+++ js/atualizar_dados_do_relatorio.js
@@ -0,0 +1,81 @@
+function atualizar_dados_do_relatorio(){
+
+    document.querySelector("#total").textContent = localStorage.getItem("valueText")
+
+    document.querySelector("#total1").textContent = localStorage.getItem("valueText1")
+
+    document.querySelector("#total2").textContent = localStorage.getItem("valueText2")
+
+    document.querySelector("#total3").textContent = localStorage.getItem("valueText3")
+
+    document.querySelector("#total4").textContent = localStorage.getItem("valueText4")
+
+    document.querySelector("#total5").textContent = localStorage.getItem("valueText5")
+
+    document.querySelector("#total6").textContent = localStorage.getItem("valueText6")
+
+    document.querySelector("#total7").textContent = localStorage.getItem("valueText7")
+
+    document.querySelector("#total8").textContent = localStorage.getItem("valueText8")
+
+    document.querySelector("#total9").textContent = localStorage.getItem("valueText9")
+
+    document.querySelector("#total10").textContent = localStorage.getItem("valueText10")
+
+    document.querySelector("#total11").textContent = localStorage.getItem("valueText11")
+
+    document.querySelector("#total12").textContent = localStorage.getItem("valueText12")
+
+    document.querySelector("#total13").textContent = localStorage.getItem("valueText13")
+
+    document.querySelector("#total14").textContent = localStorage.getItem("valueText14")
+
+    document.querySelector("#total15").textContent = localStorage.getItem("valueText15")
+
+    document.querySelector("#total16").textContent = localStorage.getItem("valueText16")
+
+    document.querySelector("#total17").textContent = localStorage.getItem("valueText17")
+
+    document.querySelector("#total18").textContent = localStorage.getItem("valueText18")
+
+    document.querySelector("#total19").textContent = localStorage.getItem("valueText19")
+
+    document.querySelector("#total20").textContent = localStorage.getItem("valueText20")
+
+    document.querySelector("#total21").textContent = localStorage.getItem("valueText21")
+
+    document.querySelector("#total22").textContent = localStorage.getItem("valueText22")
+
+    document.querySelector("#total23").textContent = localStorage.getItem("valueText23")
+
+    document.querySelector("#total24").textContent = localStorage.getItem("valueText24")
+
+    document.querySelector("#total25").textContent = localStorage.getItem("valueText25")
+
+    document.querySelector("#total26").textContent = localStorage.getItem("valueText26")
+
+    document.querySelector("#total27").textContent = localStorage.getItem("valueText27")
+
+    document.querySelector("#total28").textContent = localStorage.getItem("valueText28")
+
+    document.querySelector("#total29").textContent = localStorage.getItem("valueText29")
+
+    document.querySelector("#total30").textContent = localStorage.getItem("valueText30")
+
+    document.querySelector("#total31").textContent = localStorage.getItem("valueText31")
+
+    document.querySelector("#total32").textContent = localStorage.getItem("valueText32")
+
+    document.querySelector("#total33").textContent = localStorage.getItem("valueText33")
+
+    document.querySelector("#total34").textContent = localStorage.getItem("valueText34")
+
+    document.querySelector("#total35").textContent = localStorage.getItem("valueText35")
+
+    document.querySelector("#total36").textContent = localStorage.getItem("valueText36")
+
+    document.querySelector("#total37").textContent = localStorage.getItem("valueText37")
+    
+    document.querySelector("#total38").textContent = localStorage.getItem("valueText38")
+
+}
