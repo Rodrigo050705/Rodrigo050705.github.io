e3eaa93f29e5f0fa22efc9172b7bcff510c8096f
Rodrigo050705 <135657705+Rodrigo050705@users.noreply.github.com>
2024-01-31 15:11:31 -0300

    Add files via upload

---
+++ sem_qr.js
@@ -0,0 +1,29 @@
+total = 0;
+function somar(){
+    total = parseFloat(document.getElementById("digitar").value) + total;
+    document.getElementById("sem_qr").innerHTML = total;
+}
+
+total_1 = 0;
+function somar_1(){
+    total_1 = parseFloat(document.getElementById("digitar_1").value) + total_1;
+    document.getElementById("sem_qr_1").innerHTML = total_1;
+}
+
+total_2 = 0;
+function somar_2(){
+    total_2 = parseFloat(document.getElementById("digitar_2").value) + total_2;
+    document.getElementById("sem_qr_2").innerHTML = total_2;
+}
+
+total_3 = 0;
+function somar_3(){
+    total_3 = parseFloat(document.getElementById("digitar_3").value) + total_3;
+    document.getElementById("sem_qr_3").innerHTML = total_3;
+}
+
+total_4 = 0;
+function somar_4(){
+    total_4 = parseFloat(document.getElementById("digitar_4").value) + total_4;
+    document.getElementById("sem_qr_4").innerHTML = total_4;
+}
